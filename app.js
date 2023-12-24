499e0568c2d0368ead3a2ba2a273085f8a860a25
Tanav Poswal <63358333+tanavdev@users.noreply.github.com>
2023-12-24 11:03:09 +0530

    app made

---
+++ app.js
@@ -0,0 +1,46 @@
+const memeinp = document.querySelector("#memeinp")
+const memebtn = document.querySelector("#memebtn")
+const memeheading = document.querySelector("#memeheading")
+const memeimg = document.querySelector("#memeimg")
+const dhoni = document.getElementById("dhoni");
+const moyeaud = document.getElementById("moye");
+
+function checkINT(str) {
+    let numString = Math.abs(str).toString(); // Convert number to string
+    let sum = 0;
+
+    for (let i = 0; i < numString.length; i++) {
+        let digit = parseInt(numString[i]);
+        sum += digit;
+    }
+
+    return sum;
+}
+
+memebtn.addEventListener("click", () => {
+    if (memeinp==""){
+        
+    } else {
+    if (checkINT(memeinp.value) == 7) {
+        thala(memeinp.value)
+    } else if(memeinp.value.length === 7) {
+        thala(memeinp.value)
+    } else {
+        moye(memeinp.value)
+    }}
+
+})
+
+function thala(str) {
+    dhoni.play()
+    moyeaud.pause()
+    memeheading.innerText = `${str} is THALA for a reason.`
+    memeimg.src = "ms-dhoni.webp"
+}
+
+function moye() {
+    dhoni.pause()
+    moyeaud.play()
+    memeheading.innerText = "MOYE MOYE"
+    memeimg.src = "moye-moye.jpeg"
+}
